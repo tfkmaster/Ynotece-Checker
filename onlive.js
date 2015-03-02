c907e9b2f62f2156272e88785490fa1e05a7edde
tfkmaster <tfk47@live.fr>
2015-03-02 23:15:19 +0100

    LE Commit
    Parce que le HIHIHIHIHI

---
+++ onlive.js
@@ -0,0 +1,32 @@
+function YnoCheck()
+{
+
+	var request = new XMLHttpRequest();
+	request.open("GET", "https://api.dailymotion.com/user/Ynotece/videos?fields=broadcasting", false);
+	request.send(null);
+
+	var response = JSON.parse(request.responseText);
+	if (response.list[0].broadcasting == true)
+	{
+		document.getElementById("result").innerHTML = "♥~ PUTAIN OUAIS ELLE LIVE ! ~♥";
+		document.getElementById("result").style.color = "#0F0";
+		document.getElementById("s1").style.display = "inline";
+		document.getElementById("s2").style.display = "inline";
+		document.getElementById("s3").style.display = "none";
+		document.getElementById("s4").style.display = "none";
+		document.getElementById("yes").play();
+	}
+	else
+	{
+		document.getElementById("result").innerHTML = "Meeeh... Elle live pas...";
+		document.getElementById("result").style.color = "#F00";
+		document.getElementById("s1").style.display = "none";
+		document.getElementById("s2").style.display = "none";
+		document.getElementById("s3").style.display = "inline";
+		document.getElementById("s4").style.display = "inline";
+		document.getElementById("no").play();
+	}
+
+}
+
+//
---
+++ onlive.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ynotece - Live Checker</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="onlive.css?2">
+	<script type="text/javascript" src="onlive.js"></script>
+</head>
+<body>
+
+	<header>
+		<h1><img src=""/>Le YNOTECE Live Checker (officiellement officieux)</h1>
+		<nav style="display:none;">
+			<ul>
+				<li><a href="#">Ouais non y'a rien d'autre à voir</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<div><img id="s1" src="Yno.png"/><img id="s3" src="isaac.png"/><p style="display: inline;" id="result"></p><img id="s4" src="isaac.png"/><img id="s2" src="Yno.png"/></div>
+		<button onclick="YnoCheck();">Mais est-ce que YNOTECE est en live ?!</button>
+	</main>
+
+	<footer>
+		<p class="rainbow">Ce site veut rien dire © EL TACOS FACTORY 2015 ♥</p>
+		<audio id="yes" src="Hihihi.ogg"></audio>
+		<audio id="no" src="Naaan.ogg"></audio>
+	</footer>
+	
+</body>
